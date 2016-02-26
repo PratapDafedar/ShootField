f43cb1e242eeabacccc24b68b7dcdd2ae5949bdb
Pratap Dafedar <pratap.dafedar@juegostudio.com>
2016-02-26 11:31:11 +0530

    added uNet sample.

---
+++ Assets/ZZTest/PlayerMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMove : NetworkBehaviour
+{
+    public float speed = 15f;
+
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            GetComponent<MeshRenderer>().material.color = Color.red;
+            return;
+        }
+
+        var x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        var z = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        transform.Translate(x, 0, z, Space.World);
+    }
+}
